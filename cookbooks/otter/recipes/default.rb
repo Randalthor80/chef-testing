209d029d9e1ee4f912f52339932f30c13ebd6d84
Chris Mann <chris@walkingthumbs.com>
2014-12-04 14:17:45 -0500

    let's try this again

---
+++ cookbooks/otter/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: otter
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+file '/tmp/otter.txt' do
+	content 'otter otter'
+end
+
